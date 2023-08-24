70a467c1e7a3fb181f315f3fe8924bbef275fda9
KPANOU <126262132+KPANOU@users.noreply.github.com>
2023-08-24 11:50:46 +0200

    update

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/etienne/catkin_ws/src
+# Build directory: /home/etienne/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_mobile_robot")
+subdirs("my_cam")
+subdirs("ros_hand_gesture_recognition")
